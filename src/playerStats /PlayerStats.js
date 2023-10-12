a8588794912fd20b64e431b0a805e2dfe1a5184f
Digvijay <50370704+Digvijayhj@users.noreply.github.com>
2023-10-12 11:08:18 -0700

    Initial Commit

---
+++ src/playerStats /PlayerStats.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PlayerStats = () => {
+    return (
+        <div id="playerStats">
+            <h1>DIGVIJAY</h1>
+            <div id="playerStats__lines">
+                <span id="playerStats__lines__thick"></span>
+                <span id="playerStats__lines__thin"></span>
+            </div>
+            <h2>Fullstack Developer</h2>
+        </div>
+    );
+};
+
+export default PlayerStats;
